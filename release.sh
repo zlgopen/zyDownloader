99a102f238051bc6ecffadb4ff3529b049ee3646
lixianjing <lixianjing@localhost.localdomain>
2017-02-01 17:38:01 +0800

    fix build script for linux

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ mkdir -p release/Release/
 
 cp -fv package.json release/package.json
 cp -fv build/Release/*.node release/Release
-cp -frv node_modules/bindings release/node_modules/bindings
+cp -frvL node_modules/bindings release/node_modules/bindings
 
 for f in index.html	www
 do
